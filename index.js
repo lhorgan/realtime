32127ec50da80ad7927cabd28711df2f4c16c1a7
Luke Horgan <horgan.l@husky.neu.edu>
2020-07-02 00:23:54 -0400

    latest changes

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const puppeteer = require('puppeteer');
 const lineByLine = require('n-readlines');
-const request = require("request")
+const request = require("request");
+var AWS = require('aws-sdk');
+AWS.config.update({region: "us-west-1"});
 
 class I {
     constructor(ifname, serverURL) {
@@ -21,7 +23,7 @@ class I {
             let username = url.split("/").pop();
             console.log(username);
             
-            request.get(`${this.serverURL}/username/?username=${username}`, (err, res, body) => {
+            /*request.get(`${this.serverURL}/username/?username=${username}`, (err, res, body) => {
                 if(err || res.statusCode > 200) {
                     console.log("Something went wrong...");
                     console.log(err);
@@ -29,7 +31,7 @@ class I {
                 else {
                     console.log(this.ids[i] + "\t" + username);
                 }
-            });
+            });*/
 
             page.close();
             await this.rest(parseInt(20000 + Math.random() * 20000));
@@ -59,4 +61,57 @@ class I {
     }
 }
 
+class TweetFetcher {
+    constructor() {
+        let lambdaNum = parseInt(Math.random() * 100);
+        this.getTweets(`twint_gamma_${1}`, "potus44", 20);
+    }
+
+    getTweets(lambdaName, username, limit, until) {
+        let payload = {
+            "Username": username,
+            "Limit": limit,
+            "Store_object": true
+        }
+
+        if(until) {
+            payload["Until"] = until;
+        }
+        
+        var params = {
+            FunctionName: lambdaName,
+            Payload: JSON.stringify(payload)
+        };
+
+        var lambda = new AWS.Lambda();
+        lambda.invoke(params, (err, data) => {
+            let lambdaResp = {};
+
+            if(err) {
+                console.log("There was an error");
+                lambdaResp["error"] = true;
+            }
+            else {
+                console.log("we successed");
+                //console.log(data);
+                //lambdaResp = JSON.parse(data["Payload"])
+                //console.log(data["Payload"]);
+                //console.log(typeof(data["Payload"]));
+                let res = data["Payload"];
+                //console.log(res);
+                res = JSON.parse(res);
+                console.log(res);
+                //console.log(res.length);
+                for(let i = 0; i < res.length; i++) {
+                    let timestamp = res[i].datestamp + " " + res[i].timestamp;
+                    console.log(timestamp);
+                    console.log(res[i].id_str);
+                }
+                //cb(res);
+            }
+        });
+    }
+}
+
+//let t = new TweetFetcher();
 let e = new I("/home/luke/Documents/lazer/id_to_username/latest_sinceid_file_high_active.tsv", "http://3.80.160.1:5000");
