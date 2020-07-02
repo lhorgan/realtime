76cf9d065e73cf9c96aca8cb2a10f36c98597182
Luke Horgan <horgan.l@husky.neu.edu>
2020-07-02 04:48:37 -0400

    I don't know!  It's 5am!

--- convert_ids.js
+++ convert_ids.js
@@ -1,38 +1,37 @@
 const puppeteer = require('puppeteer');
 const lineByLine = require('n-readlines');
-const request = require("request");
+
+const asyncRedis = require("async-redis");
+const client = asyncRedis.createClient();
 
 class I {
-    constructor(ifname, serverURL) {
+    constructor(ifname, proxies) {
+        this.browsers = [];
+
         (async () => {
-            this.serverURL = serverURL;
-            this.browser = await puppeteer.launch({args: ['--no-sandbox']});
+            for(let i = 0; i < proxies.length; i++) {
+                let args = [`--no-sandbox`, `--proxy-server=${proxies[i]}`, `--ignore-certificate-errors`];
+                this.browsers.push(await puppeteer.launch({args: args}));
+            }
+
             this.ids = this.readIds(ifname);
             await this.processIds();
-            this.browser.close();
+
+            for(let i = 0; i < proxies.length; i++) {
+                this.browsers[i].close();
+            }
         })();
     }
 
     async processIds() {
-        for(let i = 1000; i < 2000; i++) {
-            let page = await this.browser.newPage();
+        for(let i = 0; i < this.ids.length; i++) {
+            let page = await this.browsers[i % this.browsers.length].newPage();
             await page.goto(`https://twitter.com/i/user/${this.ids[i]}`, {"waitUntil" : "networkidle0"});
             let url = await page.url();
             let username = url.split("/").pop();
             console.log(username);
-            
-            /*request.get(`${this.serverURL}/username/?username=${username}`, (err, res, body) => {
-                if(err || res.statusCode > 200) {
-                    console.log("Something went wrong...");
-                    console.log(err);
-                }
-                else {
-                    console.log(this.ids[i] + "\t" + username);
-                }
-            });*/
-
             page.close();
-            await this.rest(parseInt(20000 + Math.random() * 20000));
+            //await this.rest(parseInt(20000 + Math.random() * 20000));
         }
     }
 
@@ -58,3 +57,11 @@ class I {
         return list;
     }
 }
+
+let proxyList = ["35.245.138.206:3128",
+                "35.188.147.108:3128",
+                "35.222.86.237:3128",
+                "35.239.236.224:3128",
+                "35.192.135.179:3128"];
+
+let e = new I("/home/luke/Documents/lazer/id_to_username/latest_sinceid_file_high_active.tsv", proxyList);
